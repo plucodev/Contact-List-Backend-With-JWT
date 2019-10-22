fdd3ee760e332716616d0891ef4eb3fcedbeaf17
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-22 14:19:55 +0000

    added GET POST methods for person

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -27,6 +27,37 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/person', methods=['POST', 'GET'])
+def handle_person():
+    """
+    Create person and retrieve all persons
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'username' not in body:
+            raise APIException('You need to specify the username', status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+
+        user1 = Person(username=body['username'], email=body['email'], password=body['password'])
+        db.session.add(user1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_people = Person.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return jsonify(all_people), 200
+
+    return "Invalid Method", 404
+
 
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_person():
---
+++ src/test.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+list1 = ["a", "b" , "c", "a","o","h"]
+list2 = [1, 2, 3]
+
+def my_function(d):
+  d.reverse()
+  print(d[-1])
+  print(d)
+
+my_function(thislist)
