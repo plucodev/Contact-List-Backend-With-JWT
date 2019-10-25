097e60743949a44a5c7bd4716b060a67ea254969
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-25 15:00:13 +0000

    created contact endpoint

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Person
+from models import db, Person, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -51,6 +51,33 @@ def handle_person():
     # GET request
     if request.method == 'GET':
         all_people = Person.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return jsonify(all_people), 200
+
+    return "Invalid Method", 404
+@app.route('/contact', methods=['POST', 'GET'])
+def handle_contact():
+    """
+    Create person and retrieve all persons
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+
+        user1 = Contact(name=body['name'], email=body['email'], address=body['address'], phone=body['phone'])
+        db.session.add(user1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_people = Contact.query.all()
         all_people = list(map(lambda x: x.serialize(), all_people))
         return jsonify(all_people), 200
 
